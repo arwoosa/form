8e7c56c031d98b0eede80351f8a0b291946fb841
bryan lin <920616bryanlin@gmail.com>
2025-09-04 11:23:52 +0800

    feat: initialize Keto relation client and update MongoDB configuration

--- cmd/form-server/server.go
+++ cmd/form-server/server.go
@@ -11,6 +11,7 @@ import (
 	"github.com/arwoosa/form-service/internal/service"
 	"github.com/arwoosa/vulpes/ezgrpc"
 	"github.com/arwoosa/vulpes/log"
+	"github.com/arwoosa/vulpes/relation"
 )
 
 var serverCmd = &cobra.Command{
@@ -41,6 +42,24 @@ func runServer(cmd *cobra.Command, args []string) {
 	if _, err := mongodb.InitMongoDB(ctx, appConfig.MongodbConfig); err != nil {
 		log.Error("Failed to initialize MongoDB", log.Err(err))
 		log.Fatal("Form service requires MongoDB connection - cannot start without database")
+	}
+
+	// Initialize Keto relation client
+	if appConfig.KetoConfig != nil {
+		log.Info("Initializing Keto relation client",
+			log.String("write_addr", appConfig.KetoConfig.WriteAddr),
+			log.String("read_addr", appConfig.KetoConfig.ReadAddr))
+
+		relation.Initialize(
+			relation.WithWriteAddr(appConfig.KetoConfig.WriteAddr),
+			relation.WithReadAddr(appConfig.KetoConfig.ReadAddr),
+		)
+
+		// Ensure relation connection is closed when server shuts down
+		defer relation.Close()
+		log.Info("Keto relation client initialized successfully")
+	} else {
+		log.Warn("Keto configuration not found - authorization features may not work")
 	}
 
 	// Register services
--- go.mod
+++ go.mod
@@ -75,6 +75,7 @@ require (
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.1 // indirect
+	github.com/ory/keto/proto v0.13.0-alpha.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
