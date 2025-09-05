69434e4aafbfd816aeab481aa80976fbc7e8a267
Chen Yan Jui <ch.focke@gmail.com>
2025-09-05 14:37:23 +0800

    add dev portainer cicd

--- internal/constants/constants.go
+++
@@ -1,9 +0,0 @@
-package constants
-
-const (
-	LimitFeedbackCount = 20
-	LimitCommentCount  = 2
-
-	ZeroString = ""
-	ZeroInt    = 0
-)
--- pkg/vulpes/validate/validate.go
+++
@@ -1,9 +0,0 @@
-package validate
-
-import "github.com/go-playground/validator/v10"
-
-var validate = validator.New()
-
-func Struct(v any) error {
-	return validate.Struct(v)
-}
